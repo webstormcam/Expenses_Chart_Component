dc3629b4937f58f38ec7d5eb07444c59d290ff77
Cameron Chandler <webstormcam@gmail.com>
2022-08-06 00:20:47 -0400

    Making progress with assigning the nums

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let daysOfWeek = document.querySelectorAll('.stat')
+
+
 fetch("data.json")
 .then(res => res.json())
 .then(data => {
     
+    for(let i=0;i<data.length;i++){
+        daysOfWeek[i].style.height=data[i].amount
+        }
+          
     
-    console.log(data)
 });
+
