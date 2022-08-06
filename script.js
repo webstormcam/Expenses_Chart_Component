6ec87e737b271113281cfbc002e665c8d64ff26f
Cameron Chandler <webstormcam@gmail.com>
2022-08-06 09:14:08 -0400

    Made the chart dynamic

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let daysOfWeek = document.querySelectorAll('.stat')
-
+let current=0;
 
 fetch("data.json")
 .then(res => res.json())
 .then(data => {
     
     for(let i=0;i<data.length;i++){
-        daysOfWeek[i].value=data[i].amount;
+        current=data[i].amount;
         daysOfWeek[i].style.height=`${data[i].amount*2.87}px`
+        daysOfWeek[i].style.backgroundColor='#76B5BC'
+        for(let q=0;q<data.length;q++){
+            if(current<data[q].amount){
+                daysOfWeek[i].style.backgroundColor='#EC755D'
+                
+            }
+        }
+        
+        
+        
+        
+        
         }
 
-        console.log(data[0].amount)
+        
           
     
 });
