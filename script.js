d9652b379198b1a5d7b39bb9ed365ce4a2753f2c
Cameron Chandler <webstormcam@gmail.com>
2022-08-07 09:14:06 -0400

    Now I just gotta make them hide and appear

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let daysOfWeek = document.querySelectorAll('.stat')
+let moneyContainer = document.querySelector('.money')
+let moneyValue = document.querySelectorAll('.money-text')
 let current=0;
 
 fetch("data.json")
@@ -9,12 +11,15 @@ fetch("data.json")
         current=data[i].amount;
         daysOfWeek[i].style.height=`${data[i].amount*2.87}px`
         daysOfWeek[i].style.backgroundColor='#76B5BC'
+       
         for(let q=0;q<data.length;q++){
             if(current<data[q].amount){
                 daysOfWeek[i].style.backgroundColor='#EC755D'
                 
             }
         }
+
+        moneyValue[i].innerHTML=`$${data[i].amount}`
         
         
         
